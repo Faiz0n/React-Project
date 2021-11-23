c82fb6ae510bc9375118ec556782c8104f129423
Faizan Shaikh <faizanshaikh91889@gmail.com>
2021-11-23 23:22:24 +0530

    Just added the shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
+
+import {Routes,Route} from 'react-router-dom'
+
 import './App.css';
+
+import ShopPage from './pages/shopPage/shop.component';
 
 import Homepage from './pages/homepage/homepage.component';
 
+
 function App() {
   return (
-    <div >
-<Homepage/>
+    <div>
+<Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<ShopPage/>} />
+</Routes>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/compenents/menu-items/menu-item.component.jsx
+++ src/compenents/menu-items/menu-item.component.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 
+import {Link,Navigate,Route} from 'react-router-dom'
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) =>(
+const MenuItem = ({title, imageUrl, size, history, linkUrl}) =>(
     <div 
-    className={`${size} menu-item`}>
+    className={`${size} menu-item`} 
+    >
         <div className="background-image"
         style = {{
             backgroundImage:`url(${imageUrl})`
         }}
         />
               <div className='content'>
-                  <h1 className='title'>{title.toUpperCase}</h1>
-                  <span className='subtitle'>SHOP NOW</span>
+                  <h1 className='title'>{title.toUpperCase()}</h1>
+               <Link to="/shop"><span className='subtitle'>SHOP NOW</span></Link>                
               </div>
           </div>
 );
 
 
 export default MenuItem
+// onClick={()=> navigate(`${match.url}${linkUrl}`)}
