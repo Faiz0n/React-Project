9d75d3648698e9c16eefebf6d009d00d1259cefb
Faizan Shaikh <faizanshaikh91889@gmail.com>
2021-12-05 20:23:35 +0530

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/compenents/collection-item/collection-item.component.jsx
+++ src/compenents/collection-item/collection-item.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+import {connect} from 'react-redux'
+import { addItem } from "../../redux/cart/cart.actions";
+
+import CustomButton from "../custom-button/custom-button.component";
+
 import './collection-item.styles.scss' 
 
-const CollectionItem = ({id,name,imageUrl,price})=>(
+const CollectionItem = ({item,addItem})=>{
+    const {name, price, imageUrl} = item
+    return(
     <div className='collection-item'>
     <div 
     className='image'
@@ -14,8 +21,12 @@ const CollectionItem = ({id,name,imageUrl,price})=>(
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
     </div>
-);
+)};
 
+const mapDispatchToProps = dispatch =>({
+    addItem: item => dispatch(addItem(item))
+})
 
-export default CollectionItem;
+export default connect(null,mapDispatchToProps)(CollectionItem);
--- src/compenents/preview-collection/collection-preview.component.jsx
+++ src/compenents/preview-collection/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({title, items}) =>(
         <div className = 'preview'>
             {items
             .filter((item,indx)=> indx < 4)
-            .map(({id, ...otherItemPorps}) =>(
-                <CollectionItem key={id}{...otherItemPorps}/>
+            .map((item) =>(
+                <CollectionItem key={item.id} item={item}/>
             ))}
         </div>
     </div>
